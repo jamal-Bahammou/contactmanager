fb4d04cb384b35a296e3fd7fd168bb123d1266a3
Jamal Bahammou <jamalbahammou456@gmail.com>
2019-02-09 23:29:03 +0100

    Updates

---
+++ precache-manifest.cd8e1b17ac81bc8378dc986a40e753c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2bd5ce98c25e5c3ce5db",
+    "url": "/contactmanager/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "2bd5ce98c25e5c3ce5db",
+    "url": "/contactmanager/static/js/main.2bd5ce98.chunk.js"
+  },
+  {
+    "revision": "f15547e199b4bd8ab515",
+    "url": "/contactmanager/static/css/1.8675d2b5.chunk.css"
+  },
+  {
+    "revision": "f15547e199b4bd8ab515",
+    "url": "/contactmanager/static/js/1.f15547e1.chunk.js"
+  },
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "a20e6506ebb5d8677a7fd9c1bc14637d",
+    "url": "/contactmanager/index.html"
+  }
+];
